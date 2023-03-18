164466edb625cca8645e10ee5b246f50ab7c0a12
your name <simegnew62@gmail.com>
2023-03-18 13:46:21 -0500

    message schema

---
+++ backend/models/message.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  recipientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Message', messageSchema);
