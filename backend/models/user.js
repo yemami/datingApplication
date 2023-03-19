69e92610f3731f5a6af078acf8626b913c0f0c14
your name <simegnew62@gmail.com>
2023-03-19 03:00:56 -0500

    some updation on user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,6 +5,15 @@ const userSchema = new mongoose.Schema({
       type: String,
       required: true
     },
+    dateOfBirth: {
+      type: Date,
+      required: true
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female', 'other'],
+      required: true
+    },
     email: {
       type: String,
       required: true,
@@ -14,23 +23,26 @@ const userSchema = new mongoose.Schema({
       type: String,
       required: true
     },
-    age: {
-      type: Number,
-      required: true
+    profile: {
+      profilePic: { 
+        type: String,
+      },
+      bio: {
+        type: String,
+      },
+      location: [Number],
+      interests: [String],
+      likedUsers: [{
+        type: mongoose.Schema.Types.ObjectId,
+      }],
+      viewedUsers: [{
+        type: mongoose.Schema.Types.ObjectId,
+      }],
+      matchedUsers: [{
+        type: mongoose.Schema.Types.ObjectId,
+      }]
     },
-    gender: {
-      type: String,
-      enum: ['male', 'female', 'other'],
-      required: true
-    },
-    bio: {
-      type: String,
-      required: true
-    },
-    likedUsers: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
-    }]
+
   });
   
   const User = mongoose.model('User', userSchema);
