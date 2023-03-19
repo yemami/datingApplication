b2f6bb90a3209f0d437f5573d8a2ee965ba187d1
your name <simegnew62@gmail.com>
2023-03-19 02:58:50 -0500

    updated user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,64 +1,7 @@
 const bcrypt = require('bcrypt');//used to hash passwords
-const jwt = require('jsonwebtoken');//used to create, sign, and verify tokens
-const config = require('../config/config');//config file contains all tokens and other private info
 const User = require('../models/User');//
 
-module.exports.createUser = async (req, res, next) => {
-  try {
-    const { name, email, password, age, gender, bio,likedUsers } = req.body;//req.body is the data that is sent from the frontend
-
-    // Check if user already exists in database
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({
-        success: false,
-        error: 'User already exists in database with this email' + email,
-      });
-    }
-
-    // Hash password with bcrypt
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // Create new user
-    const newUser = new User({
-      name,
-      email,
-      password: hashedPassword,
-      age,
-      gender,
-      bio,
-      likedUsers
-    });
-
-    // Save user to database and return saved user
-    const user= await User.create(newUser);
-
-    return res.status(201).json({
-      success: true,
-      message: 'User successfully created in database',
-      data: user,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-module.exports. getAllUser = async (req, res, next) => {
-  try {
-    const users = await User.find();
-    return res.status(200).json({
-      success: true,
-      message: 'Users successfully retrived from database',
-      count: users.length,
-      data: users,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-module.exports. getUserById = async (req, res, next) => {
+module.exports.getUserById = async (req, res, next) => {
   try {
     const user_id = req.params.user_id;
     const result = await User.findById({ _id: user_id });
@@ -79,7 +22,7 @@ module.exports. getUserById = async (req, res, next) => {
   }
 };
 
-module.exports.updateUser = async (req, res, next) => {
+module.exports.updateCurrentUser = async (req, res, next) => {
   try {
     const user_id = req.params.user_id;
     const { name, email, password, age, gender, bio, likedUsers } = req.body;
@@ -121,7 +64,7 @@ module.exports.updateUser = async (req, res, next) => {
   }
 };
 
-module.exports.deleteUser = async (req, res, next) => {
+module.exports.deleteCurrentUser = async (req, res, next) => {
   try {
     const user_id = req.params.user_id;
     const user = await User.findById({_id:user_id});
