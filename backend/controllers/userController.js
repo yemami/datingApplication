746dfbeaa87b6b6cfcf3f621bb45a86e602bb5db
your name <simegnew62@gmail.com>
2023-03-21 03:00:36 -0500

    final updation on user contrller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,35 @@
-const bcrypt = require('bcrypt');//used to hash passwords
-const User = require('../models/User');//
+const bcrypt = require("bcrypt"); //used to hash passwords
+const User = require("../models/User"); //
 
-module.exports.getUserById = async (req, res, next) => {
+module.exports.getUserDetails = async (req, res, next) => {
   try {
-    const user_id = req.params.user_id;
-    const result = await User.findById({ _id: user_id });
+    const user_id = req.userId;
+    const result = await User.findOne(
+      { _id: user_id },
+      { dateOfBirth: 1, profile: 1, gender: 1, email: 1, name: 1 }
+    );
     if (!result) {
       return res.status(404).json({
         success: false,
-        error: 'User not found in database with this id =' + user_id,
+        error: "User not found in database with this id =" + user_id,
       });
     }
-
+    // transform user data to destrcuture profile
+    // change date format
+    const data = {
+      ...result._doc,
+      profilePic: result.profile.profilePic ? result.profile.profilePic : "",
+      bio: result.profile.bio ? result.profile.bio : "",
+      location: result.profile.location,
+      interests: result.profile.interests,
+      dateOfBirth: result.dateOfBirth.toISOString().split("T")[0],
+    };
+    delete data.profile;
     return res.status(200).json({
       success: true,
-      message: 'User successfully retrive with id = ' + user_id + ' from database',
-      data: result,
+      message:
+        "User successfully retrive with id = " + user_id + " from database",
+      data: data,
     });
   } catch (err) {
     next(err);
@@ -24,66 +38,20 @@ module.exports.getUserById = async (req, res, next) => {
 
 module.exports.updateCurrentUser = async (req, res, next) => {
   try {
-    const user_id = req.params.user_id;
-    const { name, email, password, age, gender, bio, likedUsers } = req.body;
-
-    let user = await User.findById({_id:user_id});
-
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        error: 'User not found in database with this id =' + user_id,
-      });
-    }
-
-    // Update user fields
-    user.name = name;
-    user.email = email;
-    user.age = age;
-    user.gender = gender;
-    user.bio = bio;
-    user.likedUsers = likedUsers;
-
-    if (password) {
-      // Hash password
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      user.password = hashedPassword;
-    }
+    const user_id = req.userId;
+    let user = await User.findOne({ _id: user_id });
 
     // Save user to database
-    await user.save();
+    const user_data = req.body;
+
+    const updatedUser = await User.updateOne({ _id: user_id }, user_data);
 
     return res.status(200).json({
       success: true,
-      message: 'User successfully updated in database',
-      data: user,
+      message: "User successfully updated in database",
+      data: updatedUser,
     });
   } catch (err) {
     next(err);
   }
 };
-
-module.exports.deleteCurrentUser = async (req, res, next) => {
-  try {
-    const user_id = req.params.user_id;
-    const user = await User.findById({_id:user_id});
-
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        error: 'User not found in database with this id =' + user_id,
-      });
-    }
-
-    await user.remove();
-    return res.status(200).json({
-      success: true,
-      message: 'User successfully deleted from database',
-      data: {},
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
