8ac374b6ecff1817a78702db2cd995134cc2341c
your name <simegnew62@gmail.com>
2023-03-21 02:58:28 -0500

    final config.js push

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    PORT: process.env.PORT || 4000,
-    JWT_SECRET: process.env.JWT_SECRET || 'secret',
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/datingapp',
-  };
+  BASE_URL: process.env.BASE_URL || "http://localhost:8000/",
+  PORT: process.env.PORT || 8000,
+  JWT_SECRET: process.env.JWT_SECRET || "secret",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/datingapp",
+};
