4d99c0df5eea0283c0cae5b96486c89231dd2030
your name <simegnew62@gmail.com>
2023-03-21 03:05:47 -0500

    chat routes

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const chatController = require('../controllers/chatController');
-const { authMiddleware } = require('../middlewares/authMiddleware');
+const chatController = require("../controllers/chatController");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
 // Get all chats for the logged in user
-router.get('/', authMiddleware, chatController.getAllChats);
+router.get("/", authMiddleware, chatController.getAllChats);
 
 // Get chat messages between two users
 // router.get('/:userId', authMiddleware, chatController.getChatMessages);
 
 // Send a message to a user
-router.post('/:userId', authMiddleware, chatController.sendMessage);
+router.post("/:chatId", authMiddleware, chatController.sendMessage);
+router.get("/:chatId", authMiddleware, chatController.getChatMessages);
 
 module.exports = router;
