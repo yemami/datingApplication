1830cf51795aa64221b093c5b6c569f016ec92e0
your name <simegnew62@gmail.com>
2023-03-21 03:13:37 -0500

    storage functionality used to uploading

---
+++ backend/utility/storage.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadDir = "./uploads";
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const fileName = `${file.fieldname}-${Date.now()}${ext}`;
+    cb(null, fileName);
+  },
+});
+const upload = multer({ storage: storage });
+module.exports = upload;
