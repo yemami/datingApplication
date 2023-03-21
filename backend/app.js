63361c75331a99f79286e1c3f315a6b758abd3a9
your name <simegnew62@gmail.com>
2023-03-21 03:09:01 -0500

    main app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const fs = require("fs");
 
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const searchRoutes = require("./routes/searchRoutes");
+
 const config = require("./config/config");
 
 (async function () {
@@ -19,13 +19,17 @@ const config = require("./config/config");
     console.log(`Error connecting to DB`, e);
   }
 })();
+
 const app = express();
+// setting middlewaes
 app.use(cors());
+app.use(express.static("uploads"));
 app.use(express.json());
+// set static folder
 
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
-app.use("/chat", chatRoutes);
+app.use("/chats", chatRoutes);
 app.use("/profile", profileRoutes);
 app.use("/search", searchRoutes);
 
@@ -33,6 +37,7 @@ app.all("*", (req, res, next) => {
   next(new Error("No route found"));
 });
 
+// start server
 app.listen(config.PORT, () => console.log(`listening on ${config.PORT}`));
 
 process.on("exit", () => {
