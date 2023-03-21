8baa34eec8c35ae34d132b16389a5cbeb667029d
your name <simegnew62@gmail.com>
2023-03-21 03:05:20 -0500

    auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require("../controllers/authController");
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
 
 module.exports = router;
