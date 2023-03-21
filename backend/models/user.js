79464514dcf5f336300a4842bbd417a3b6641146
your name <simegnew62@gmail.com>
2023-03-21 03:04:08 -0500

    final updated user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,50 +1,71 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "other"],
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profile: {
+    profilePic: {
       type: String,
-      required: true
     },
-    dateOfBirth: {
-      type: Date,
-      required: true
-    },
-    gender: {
+    bio: {
       type: String,
-      enum: ['male', 'female', 'other'],
-      required: true
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true
+    location: {
+      type: [Number, Number], // [longitude, latitude]
     },
-    password: {
-      type: String,
-      required: true
-    },
-    profile: {
-      profilePic: { 
-        type: String,
+    interests: [String],
+    likedUsers: [
+      {
+        _id: mongoose.Schema.Types.ObjectId,
+        name: String,
+        age: Number,
+        bio: String,
+        location: String,
+        profilePic: String,
       },
-      bio: {
-        type: String,
+    ],
+    viewedUsers: [
+      {
+        _id: mongoose.Schema.Types.ObjectId,
+        name: String,
+        age: Number,
+        bio: String,
+        location: String,
+        profilePic: String,
       },
-      location: [Number],
-      interests: [String],
-      likedUsers: [{
-        type: mongoose.Schema.Types.ObjectId,
-      }],
-      viewedUsers: [{
-        type: mongoose.Schema.Types.ObjectId,
-      }],
-      matchedUsers: [{
-        type: mongoose.Schema.Types.ObjectId,
-      }]
-    },
-
-  });
-  
-  const User = mongoose.model('User', userSchema);
-  
-  module.exports = User;
+    ],
+    matchedUsers: [
+      {
+        _id: mongoose.Schema.Types.ObjectId,
+        name: String,
+        age: Number,
+        bio: String,
+        location: String,
+        profilePic: String,
+      },
+    ],
+  },
+});
+userSchema.index({ "profile.location": "2d" });
+const User = mongoose.model("User", userSchema);
+module.exports = User;
