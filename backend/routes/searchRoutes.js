79e2a7aae7a173905acfbc9fba8cd083c437f552
your name <simegnew62@gmail.com>
2023-03-21 03:07:41 -0500

    updated search routes

--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { authMiddleware } = require('../middlewares/authMiddleware');
-const searchController = require('../controllers/searchController');
+const { authMiddleware } = require("../middlewares/authMiddleware");
+const searchController = require("../controllers/searchController");
 
 // Search users
 // PATCH update current user's liked users
-router.patch('/like-user/:user_id', authMiddleware, searchController.likeUser);
+router.post("/like-user", authMiddleware, searchController.likeUser);
 
 // GET suggested matches for current user
-router.get('/suggested-matches', authMiddleware, searchController.getSuggestedMatches);
+router.get(
+  "/suggested-matches",
+  authMiddleware,
+  searchController.getSuggestedMatches
+);
 
 module.exports = router;
